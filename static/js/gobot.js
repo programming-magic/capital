87fa91f2dc014ace444383132a321536c9764669
Sudono Tanjung <sudtanj.business@gmail.com>
2019-06-06 07:28:36 +0700

    Create gobot.js

---
+++ static/js/gobot.js
@@ -0,0 +1,10 @@
+(function(){
+window['gobot'] = window['gobot'] || function(){(window['gobot'].queue = window['gobot'].queue || []).push(arguments)}
+var script = document.createElement('script')
+script.async = 1
+script.src = 'https://www.getgobot.com/app/v1/gobot-client.js'
+var insert = document.getElementsByTagName('script')[0]
+insert.parentNode.insertBefore(script, insert)
+})()
+gobot('create', '-Lgc0rse48USPwKW9Ula')
+gobot('pageview')
